6ee613b43de9ad1b4090e45044074d300d857f1d
Joe Nash <josephdavidnash@gmail.com>
2026-03-31 09:05:45 -0600

    Fix routing

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PANELTEST.GITHUB.IO/', // <-- REPLACE THIS WITH YOUR EXACT REPO NAME!
+  base: '/',
 })
